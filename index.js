8ab8ae02ccdf16fe793c93ba5a817a84bf9a80c7
unknown <alvinrikardo6@gmail.com>
2025-07-28 08:31:54 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,102 @@
+import express from "express";
+import { projects } from "./src/assets/js/projects.js";
+import multer from "multer";
+import { Pool } from 'pg'
+import path from "path";
+
+
+const db = new Pool({
+    user: 'postgres',
+    password: 'Development1',
+    host: 'localhost',
+    port: 5432,
+    database: 'my_portfolio',
+    max: 20,
+})
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './src/assets/uploads/images')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + Date.now() + path.extname(file.originalname))
+    }
+})
+const upload = multer({ storage });
+
+db.connect()
+const app = express();
+const port = 3000;
+
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+app.use("/assets", express.static("src/assets"));
+
+app.get("/", home)
+
+async function home(req, res) {
+    try {
+        const result = await db.query(`SELECT * FROM projects ORDER BY id DESC`);
+
+        const projects = result.rows.map((project) => ({
+            ...project,
+            tech: project.tech || [],
+            isGithubPrivate: project.is_github_private === true,
+            demoUrl: project.demo_url
+        }));
+        // console.log(projects)
+
+        res.render("home", { projects });
+    } catch (error) {
+        console.error("Gagal mengambil data project:", error.message);
+        res.status(500).send("Internal Server Error");
+    }
+}
+
+
+
+
+app.get("/add_project", (req, res) => {
+    res.render("add_project");
+});
+
+app.post("/add_project", upload.single('imageProject'), handleAdd);
+
+async function handleAdd(req, res) {
+    try {
+        const { title, description, githubUrl, demoUrl, tech, isGithubPrivate } = req.body;
+        const imageName = req.file.filename;
+
+        const techList = tech.split(",").map((t) => t.trim());
+
+        // console.log({
+        //     title,
+        //     description,
+        //     githubUrl,
+        //     demoUrl,
+        //     techList,
+        //     isGithubPrivate,
+        //     fileReceived: !!req.file
+        // });
+
+
+        await db.query(
+            `INSERT INTO projects (title, description, image_data, github_url, demo_url, tech, is_github_private)
+         VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+            [title, description, imageName, githubUrl, demoUrl, techList, isGithubPrivate ? true : false]
+        );
+
+        res.redirect("/");
+    } catch (error) {
+        console.error("Gagal simpan project:", error.message);
+        res.status(500).send("Server error");
+    }
+}
+
+
+
+app.listen(port, () => {
+    console.log(`Server on http://localhost:${port}`);
+});
---
+++ src/assets/js/projects.js
@@ -0,0 +1,32 @@
+export const projects = [
+    {
+        title: "Wayslink",
+        image: "wayslink.png",
+        description:
+            "Ways-Link is a social media reference landing page that allows users to share multiple links in one profile and manage them dynamically.",
+        tech: ["React JS", "Express", "Material UI", "MySQL"],
+        githubUrl: "https://github.com/Richardo33/wayslink-client",
+        demoUrl: "https://wayslink-demo.vercel.app",
+        isGithubPrivate: false,
+    },
+    {
+        title: "Window of World",
+        image: "wow.png",
+        description:
+            "WoW is an application that provides user to read book online. User also allowed to add books to their list, but they're required to subscribe first.",
+        tech: ["React JS", "Express", "Material UI", "MySQL"],
+        githubUrl: "https://github.com/Richardo33/wow-client",
+        demoUrl: "https://wow-reader.vercel.app",
+        isGithubPrivate: false,
+    },
+    {
+        title: "404 Error",
+        image: "johLiebert.jpeg",
+        description:
+            "A website to handle server error",
+        tech: ["Vue.js", "Node.js", "Sequelize", "PostgreSQL"],
+        githubUrl: "",
+        demoUrl: "#",
+        isGithubPrivate: true,
+    },
+];  
